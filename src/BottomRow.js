20504a3aa894e6303451b005cb152d5abc2a4ff8
Jessica <rayne1993@gmail.com>
2019-11-16 11:31:55 -0600

    added more buttons

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -3,6 +3,9 @@ import "./App.css";
 
 const BottomRow = () => {
   const [count, setCount] = useState(0);
+  const [ball, setBall] = useState(0);
+  const [togo, setTogo] = useState(0);
+  const [down, setDown] = useState(0);
 
     const quarterCount = event => {
       setCount( count + 1)
@@ -10,24 +13,49 @@ const BottomRow = () => {
         setCount(0)
       }
     };
+
+    const ballCount = event => {
+      setBall( ball + 1 )
+      if (ball > 119) {
+        setBall(0)
+      }
+    };
+
+    const togoCount = event => {
+      setTogo( togo + 1 )
+      if (togo > 119) {
+        setTogo(0)
+      }
+    };
+
+    const downCount = event => {
+      setDown( down + 1)
+      if (down > 3) {
+        setDown(0)
+      }
+    };
+
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
+        <button className="quarterButton" style={{background:'black', marginTop:'20px', borderRadius:'5px'}} onClick={downCount}>Down</button>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">{togo}</div>
+        <button className="quarterButton" style={{background:'black', marginTop:'20px', borderRadius:'5px'}} onClick={togoCount}>To Go</button>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">{ball}</div>
+        <button className="quarterButton" style={{background:'black', marginTop:'20px', borderRadius:'5px'}} onClick={ballCount}>Ball On</button>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
         <div className="quarter__value">{count}</div>
-        <button className="quarterButton" style={{marginTop:'20px', borderRadius:'5px'}} onClick={quarterCount}>Quarter</button>
+        <button className="quarterButton" style={{background:'black', marginTop:'20px', borderRadius:'5px'}} onClick={quarterCount}>Quarter</button>
       </div>
     </div>
   );
